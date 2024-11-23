bae5d954b01568a0786ca356a328c52c15260df6
Robbin "Roboroads" Schepers <robbin@deschepers.nl>
2024-11-23 22:04:06 +0100

    chore: Basics

---
+++ README.md
@@ -0,0 +1,3 @@
+# Streamline <img src="static/logo.svg" width="200" align="right" />
+
+Simple broadcasting solution for online radio DJs.
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,20 @@
+use tauri::{Manager};
+
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let mut builder = tauri::Builder::default();
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app.get_webview_window("main")
+                       .expect("no main window")
+                       .set_focus();
+        }));
+    }
+
+    builder
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
